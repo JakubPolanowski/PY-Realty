9bf471345238215d09b727530d0753ae7947d090
Jakub Polanowski <jakubpol27@gmail.com>
2022-12-02 00:44:50 -0500

    added defaults

---
+++ src/realty/zillow/defaults.py
@@ -0,0 +1,13 @@
+URL = "https://www.zillow.com/search/GetSearchPageState.htm"
+# The URL for the API request
+
+HEADER = {  # Headers to be passed in the request (required to get results)
+    "authority": "www.zillow.com",
+    "accept": "*/*",
+    "accept-language": "en-US,en;q=0.9",
+    "sec-fetch-dest": "empty",
+    "sec-fetch-mode": "cors",
+    "sec-fetch-site": "same-origin",
+    "sec-gpc": "1",
+    "user-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36"
+}
