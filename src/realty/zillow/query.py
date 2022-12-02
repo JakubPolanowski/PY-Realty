f22efe4236ba2940ca4ee24f728c069cfd299012
Jakub Polanowski <jakubpol27@gmail.com>
2022-12-02 15:52:37 -0500

    implemented set filter preset

--- src/realty/zillow/query.py
+++ src/realty/zillow/query.py
@@ -1,6 +1,6 @@
 import json
 import requests
-from typing import Dict
+from typing import Dict, Any, List, Literal, Union, Set
 from . import defaults
 
 
@@ -87,8 +87,193 @@ class Query:
         self.sub_parms["isMapVisible"] = map_visible
         return self
 
-    def set_filter(self):
-        return self  # TODO
+    def set_filter(self, filter_dict: Dict[str, Any]) -> 'Query':
+        """Sets the filter state. This works by expanding the filter_dict from {key: value} to {key: {'value': value}} which is the format that the API accepts.
+
+        Args:
+            filter_dict (Dict[str, Any]): Filter parameters given as a dictionary of keys and values
+
+        Returns:
+            Query: Returns self
+        """
+
+        self.sub_parms["filterState"] = {
+            k: {"value": v} for k, v in filter_dict.items()}
+        return self
+
+    def set_filter_preset(
+            self,
+            for_sale: bool = True,
+            all_homes: bool = True,
+            sale_options: Set[Literal[
+                "ForSaleByAgent",
+                "ForSaleByOwner",
+                "NewConstruction",
+                "ComingSoon",
+                "Auction",
+                "ForSaleForeclosure",
+                "RecentlySold"
+            ]] = {"ForSaleByAgent", "ForSaleByOwner", "NewConstruction", "ComingSoon", "Auction", "ForSaleForeclosure"},
+            solid_in_last_x_days: Union[None, int] = None,
+            price_max: Union[None, int] = None,
+            price_min: Union[None, int] = None,
+            monthly_payment_max: Union[None, int] = None,
+            monthly_payment_min: Union[None, int] = None,
+            beds: Union[None, int] = None,
+            baths: Union[None, int] = None,
+            home_type: Set[Literal[
+                "SingleFamily",
+                "Townhouse",
+                "MultiFamily",
+                "Condo",
+                "LotLand",
+                "Apartment",
+                "Manufactured",
+                "ApartmentOrCondo",
+            ]] = {"SingleFamily", "Townhouse", "MultiFamily", "Condo", "LotLand", "Apartment", "Manufactured", "ApartmentOrCondo"},
+            hoa: Union[None, int] = None,
+            parking_spots: Union[None, int] = None,
+            features: Set[Literal[
+                "Garage",
+                "BasementFinished",
+                "BasementUnfinished",
+                "SingleStory",
+                "AgeRestricted55Plus",
+                "AirConditioning",
+                "Pool",
+                "Waterfront",
+                "CityView",
+                "ParkView",
+                "MountainView",
+                "WaterView",
+            ]] = set(),
+            house_sqft_max: Union[None, int] = None,
+            house_sqft_min: Union[None, int] = None,
+            lot_sqft_max: Union[None, int] = None,
+            lot_sqft_min: Union[None, int] = None,
+            year_built_start: Union[None, int] = None,
+            year_built_end: Union[None, int] = None,
+            keywords: Set[str] = set(),
+    ):
+
+        # TODO refactor export logic to helpers and constants
+
+        filter_state = {"isAllHomes": all_homes}
+
+        if for_sale:
+
+            possible_sale_options = {"ForSaleByAgent", "ForSaleByOwner", "NewConstruction",
+                                     "ComingSoon", "Auction", "ForSaleForeclosure", "RecentlySold"}
+
+            diff = possible_sale_options - set(sale_options)
+            for opt in diff:
+                filter_state[f"is{opt}"] = {"value": False}
+
+            if solid_in_last_x_days:
+                filter_state["doz"] = {"value": str(solid_in_last_x_days)}
+
+        else:
+            filter_state["isForRent"] = True
+            filter_state["isForSaleByAgent"] = {"value": False}
+            filter_state["isForSaleByOwner"] = {"value": False}
+            filter_state["isNewConstruction"] = {"value": False}
+            filter_state["isComingSoon"] = {"value": False}
+            filter_state["isAuction"] = {"value": False}
+            filter_state["isForSaleForeclosure"] = {"value": False}
+
+        if price_max or price_min:
+            price = {}
+            if price_max:
+                price["max"] = price_max
+            if price_min:
+                price["min"] = price_min
+
+            filter_state["price"] = price
+
+        if monthly_payment_max or monthly_payment_min:
+            monthly = {}
+            if monthly_payment_max:
+                monthly["max"] = monthly_payment_max
+            if monthly_payment_min:
+                monthly["min"] = monthly_payment_min
+
+            filter_state["monthlyPayment"] = monthly
+
+        if beds:
+            filter_state["beds"] = {"min": beds}
+
+        if baths:
+            filter_state["baths"] = {"min": baths}
+
+        if home_type:
+            possible_home_types = {"SingleFamily", "Townhouse", "MultiFamily",
+                                   "Condo", "LotLand", "Apartment", "Manufactured", "ApartmentOrCondo"}
+
+            diff = possible_home_types - set(home_type)
+            for opt in diff:
+                filter_state[f"is{opt}"] = {"value": False}
+
+        if hoa:
+            filter_state["hoa"] = {"max": hoa}
+
+        if parking_spots:
+            filter_state["parkingSpots"] = {"min": parking_spots}
+
+        if features:
+            feature_dict = {
+                "Garage": "hasGarage",
+                "BasementFinished": "isBasementFinished",
+                "BasementUnfinished": "isBasementUnfinished",
+                "SingleStory": "singleStory",
+                "AgeRestricted55Plus": "ageRestricted55Plus",
+                "AirConditioning": "hasAirConditioning",
+                "Pool": "hasPool",
+                "Waterfront": "isWaterfront",
+                "CityView": "isCityView",
+                "ParkView": "isParkView",
+                "MountainView": "isMountainView",
+                "WaterView": "isWaterView",
+            }
+
+            for f in features:
+                if f_key := feature_dict.get(f, None):
+                    filter_state[f_key] = {"value": True}
+
+        if house_sqft_max or house_sqft_min:
+            house_sqft = {}
+            if house_sqft_max:
+                house_sqft["max"] = house_sqft_max
+            if house_sqft_min:
+                house_sqft["min"] = house_sqft_min
+
+            filter_state["sqft"] = house_sqft
+
+        if lot_sqft_max or lot_sqft_min:
+            lot_sqft = {}
+            if lot_sqft_max:
+                lot_sqft["max"] = lot_sqft_max
+            if lot_sqft_min:
+                lot_sqft["min"] = lot_sqft_min
+
+            filter_state["lotSize"] = lot_sqft
+
+        if year_built_start or year_built_end:
+            year_range = {}
+            if year_built_start:
+                year_range["min"] = year_built_start
+            if year_built_end:
+                year_range["max"] = year_built_end
+
+            filter_state["built"] = year_range
+
+        if keywords:
+            filter_state["keywords"] = {
+                "value": f"{' ,'.join(keywords)}"
+            }
+
+        self.sub_parms["filterState"] = filter_state
+
+        return self
 
     def set_list_visable(self, list_visible: bool) -> 'Query':
         # TODO determine what impact this has
