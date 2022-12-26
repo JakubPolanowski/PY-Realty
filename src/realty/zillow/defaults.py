8288d5c0c270a8a9ba923541d23aa64ded079807
Jakub Polanowski <jakubpol27@gmail.com>
2022-12-26 17:32:37 -0500

    added graphql header

--- src/realty/zillow/defaults.py
+++ src/realty/zillow/defaults.py
@@ -11,3 +11,17 @@ HEADER = {  # Headers to be passed in the request (required to get results)
     "sec-gpc": "1",
     "user-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36"
 }
+
+GRAPHQL_HEADER = {
+    "authority": "www.zillow.com",
+    "accept": "*/*",
+    "accept-language": "en-US,en;q=0.7",
+    "client-id": "vertical-living",
+    "content-type": "application/json",
+    "origin": "https://www.zillow.com",
+    "sec-fetch-dest": "empty",
+    "sec-fetch-mode": "cors",
+    "sec-fetch-site": "same-origin",
+    "sec-gpc": "1",
+    "user-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36"
+}
