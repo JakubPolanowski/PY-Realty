bd6614419b88b56df864a5d12b492deb2da8a6b6
Jakub Polanowski <jakubpol27@gmail.com>
2022-12-25 21:43:03 -0500

    added scrape function to init

--- src/realty/zillow/details/__init__.py
+++ src/realty/zillow/details/__init__.py
@@ -1,3 +1,4 @@
 from .rental_apartment import Rental_Apartment
 from .rental_home import Rental_Home
 from .sale import Sale
+from .scrape import scrapes_listing, scrape_listings, lazy_scrape_listings
