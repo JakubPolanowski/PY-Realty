9c724a9782c8524e48293630bed6aaae8ee507c2
Jakub Polanowski <jakubpol27@gmail.com>
2023-01-02 02:14:25 -0500

    implemented test sale

--- tests/test_realtor.py
+++ tests/test_realtor.py
@@ -60,4 +60,70 @@ class TestSalesQuery:
 
 
 class TestSales:
-    ...  # TODO
+    @staticmethod
+    @pytest.fixture(scope="class")
+    def sale_subset(reasonable_sale_results):
+        return [
+            Sale(f"https://www.realtor.com/realestateandhomes-detail/{r['permalink']}") for r in reasonable_sale_results['results'][:3]
+        ]
+
+    @staticmethod
+    def test_init(sale_subset):
+        # just a basic check if init doesn't run into errors
+        sale_subset
+
+    @staticmethod
+    def test_get_loan_estimates(sale_subset):
+        p: Sale
+        for p in sale_subset:
+            assert isinstance(p.get_loan_estimates(p.price, round(
+                p.price*.2), p.fips, p.state_code, p.yearly_property_tax, p.hoa_fee), dict)
+
+    @staticmethod
+    def test_get_estimate_monthly_payment(sale_subset):
+        p: Sale
+        for p in sale_subset:
+            assert isinstance(p.get_estimated_monthly_payment(), float)
+
+    @staticmethod
+    def test_get_noise_metrics(sale_subset):
+        p: Sale
+        for p in sale_subset:
+            assert isinstance(p.get_noise_metrics(
+                p.latitude, p.longitude), dict)
+
+    @staticmethod
+    def test_get_flood_risk(sale_subset):
+        p: Sale
+        for p in sale_subset:
+            assert isinstance(p.get_flood_risk(p.property_id), dict)
+
+    @staticmethod
+    def test_get_fire_risk(sale_subset):
+        p: Sale
+        for p in sale_subset:
+            assert isinstance(p.get_fire_risk(p.property_id), dict)
+
+    @staticmethod
+    def test_get_value_estimates(sale_subset):
+        p: Sale
+        for p in sale_subset:
+            assert isinstance(p.get_value_estimates(p.property_id), dict)
+
+    @staticmethod
+    def test_get_nearby_home_values(sale_subset):
+        p: Sale
+        for p in sale_subset:
+            assert isinstance(p.get_nearby_home_values(p.property_id), list)
+
+    @staticmethod
+    def test_get_similar_homes(sale_subset):
+        p: Sale
+        for p in sale_subset:
+            assert isinstance(p.get_similar_homes(p.property_id), dict)
+
+    @staticmethod
+    def test_get_homes_in_area_with_price(sale_subset):
+        p: Sale
+        for p in sale_subset:
+            assert isinstance(p.get_homes_in_area_with_price(p.zip, p.price*.8, p.price*1.2), dict)
