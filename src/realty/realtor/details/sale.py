c27643971aad176790c8c122522f44bbd3894625
Jakub Polanowski <jakubpol27@gmail.com>
2023-01-01 20:46:33 -0500

    implemented additional attributes

--- src/realty/realtor/details/sale.py
+++ src/realty/realtor/details/sale.py
@@ -27,6 +27,51 @@ class Sale:
         self.initial_state = self.ndata['props']['pageProps']['initialState']
         self.property_details = self.initial_state['propertyDetails']
 
+        # description
+        self.description = self.property_details['description']
+        self.location = self.property_details['location']
+
+        # quick access values
+
+        self.listing_date: str = self.property_details['listing_date']
+
+        self.status: str = self.property_details['status']
+        self.price: int = self.property_details['list_price']
+        self.price_per_sqft: int = self.property_details['price_per_sqft']
+        self.yearly_property_tax: Number = self.property_details['source']['raw']['tax_amount']
+        self.year_built: int = self.description['year_built']
+        self.open_houses: Any = self.property_details['open_houses']
+
+        self.listing_description = self.description['text']
+        self.details = self.property_details['details']
+        self.beds: int = self.description['beds']
+        self.baths: int = self.description['baths']
+        self.garage: int = self.description['garage']
+        self.interior_sqft: int = self.description['sqft']
+        self.lot_sqft: int = self.description['lot_sqft']
+
+        self.address = self.location['address']
+        self.city = self.address['city']
+        self.state_code = self.address['state_code']
+        self.state = self.address['state']
+        self.county = self.address['county']
+        self.latitude = self.address['coordinate']['lat']
+        self.longitude = self.address['coordinate']['lon']
+        self.zip = self.address['postal_code']
+        self.street_address = f"{self.address['line']}, {self.city}, {self.state_code} {self.zip}"
+        self.fips: str = self.location['county']['fips_code']
+
+        self.hoa_fee: int = self.property_details['hoa'].get('fee', 0)
+
+        self.property_history = self.property_details['property_history']
+        self.tax_history = self.property_details['tax_history']
+
+        self.area_market_status = self.location.get('postal_code', {}).get(
+            'geo_statistics', {}).get('housing_market')
+
+        self.noise: str = self.get_noise_metrics(
+            self.latitude, self.longitude).get('local_text', 'Unknown')
+
     @staticmethod
     def get_page(url: str, headers: Dict = defaults.HEADER) -> requests.Response:
         """Submits a GET request to the detail URL to get the page.
@@ -70,3 +115,86 @@ class Sale:
         ndata = json.loads(ndata)
 
         return ndata
+
+    @staticmethod
+    def get_loan_estimates(
+        home_price: int,
+        down_payment: int,
+        fips: str,
+        state_code: str,
+        yearly_property_tax: int | float,
+        hoa_fee: int = 0,
+    ) -> Dict[str, Any]:
+        """Gets the loan estimates from Realtor.com
+
+        Args:
+            home_price (int): The home price
+            down_payment (int): The downpayment amount. Note that Realtor.com typically uses 20%, smaller downpayments may not be handled by Realtor.com's API correctly.
+            fips (str): The FIPS (Federal Information Processing System) code 
+            state_code (str): The state code
+            yearly_property_tax (int | float): The yearly property tax ammount
+            hoa_fee (int, optional): The hoa fee. Defaults to 0.
+
+        Returns:
+            Dict[str, Any]: Returns the API response json
+        """
+
+        url = "https://www.realtor.com/api/v1/payments/calculate_property_loan"
+
+        querystring = {
+            "hoa_fees": hoa_fee,
+            "fips": fips,
+            "state": state_code,
+            "home_price": home_price,
+            "down_payment": down_payment,
+            "veterans_benefits": "false",
+            "property_tax": yearly_property_tax,
+            "is_fees_included": "true",
+            "app_name": "realtor_dot_com",
+            "app_version": "0.0.1"
+        }
+
+        response = requests.request("GET", url, params=querystring)
+        return response.json()['results']
+
+    def get_estimated_monthly_payment(self, down_payment: int = None) -> Number:
+        """Gets the estimated monthly payment.
+
+        Args:
+            down_payment (int, optional): The downpayment on the purchase. If set to None, will default to 20% of the home price. Defaults to None.
+
+        Returns:
+            Number: Returns the monthly payment estimate
+        """
+
+        if down_payment is None:
+            down_payment = round(self.price * .2)
+
+        results = self.get_loan_estimates(
+            self.price,
+            down_payment,
+            self.fips,
+            self.state_code,
+            self.yearly_property_tax,
+            self.hoa_fee
+        )
+
+        return results['mortgage_data']['monthly_payment']
+
+    @staticmethod
+    def get_noise_metrics(latitude: float, longitude: float) -> Dict[str, Any]:
+        """Gets the noise metrics for the coordinates specified.
+
+        Args:
+            latitude (float): The latitude
+            longitude (float): The longitude
+
+        Returns:
+            Dict[str, Any]: The noise metrics
+        """
+
+        noise_query = f"https://www.realtor.com/api/v1/maps/gstat/noise?lat={latitude}&lon={longitude}"
+
+        response = requests.get(noise_query).json()
+
+        return response['result']
